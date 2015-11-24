325ecda4ad87256aa343ffe0c3e031a404c63b13
S00156762 <S00156762@mail.itsligo.ie>
2015-11-24 09:06:14 +0000

    initial commit

---
+++ myVetList/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myVetList
+{
+    class Animal
+    {
+        public static int animalCount = 0;
+
+        public Animal()
+        {
+            animalCount++;
+        }
+
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        private DateTime _dob;
+        public DateTime DOB { get { return _dob; }
+            set
+            {
+                if (_dob < DateTime.Now)
+                {
+                    _dob = value;
+                }
+                else
+                {
+                    _dob = DateTime.Now;
+                };
+            } }
+
+        public override string ToString()
+        {
+            return String.Format("{0} Born on: {1:d}", Name, DOB);
+        }
+    }
+}
---
+++ myVetList/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace myVetList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Animal> vetList = new ObservableCollection<Animal>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            vetList.Add(new Animal() { DOB = DateTime.Parse("10/07/2011"), Name = "Champion"});
+            vetList.Add(new Animal() { DOB = DateTime.Parse("25/06/2009"), Name = "Daisy"});
+
+            lbxAnimals.ItemsSource = vetList;
+            tbkNumAnimals.Text = Animal.animalCount.ToString();
+        }
+
+        private void btnAddAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            string nme = tbxName.Text;
+            string db = tbxDOB.Text;
+
+            vetList.Add(new Animal() { DOB = DateTime.Parse(db), Name = nme});
+
+            lbxAnimals.ItemsSource = vetList;
+            tbkNumAnimals.Text = Animal.animalCount.ToString();
+        }
+    }  // end mainWindow class
+}  // end ns
